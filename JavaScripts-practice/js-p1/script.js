7ce864ed145b88bec6ff6fdf57f7848ca36eccc7
Manthan Thakor <thakormanthan849@gmail.com>
2024-03-19 11:59:10 +0530

    js p1.1

---
+++ JavaScripts-practice/js-p1/script.js
@@ -0,0 +1,18 @@
+alert("Hello World");
+console.log("Code is running...")
+console.log("Code is still running...")
+console.log("Code is op...")
+
+var a = prompt("Enter your number")
+var isTrue = confirm("Are you sure you want to leave this page and danger your computer ")
+if(isTrue){
+    console.log("Computer is in danger")
+}
+
+else{
+    console.log("Computer is not in danger")
+
+}
+console.log("Your number is " + a)
+
+document.title = "Hey I am good"
