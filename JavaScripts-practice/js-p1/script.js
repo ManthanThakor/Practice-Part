80e55b9a34c3de6a8f8f986ff9928a8e688496fc
Manthan Thakor <thakormanthan849@gmail.com>
2024-03-19 11:59:34 +0530

    js p1.2

--- JavaScripts-practice/js-p1/script.js
+++ JavaScripts-practice/js-p1/script.js
@@ -2,7 +2,6 @@ alert("Hello World");
 console.log("Code is running...")
 console.log("Code is still running...")
 console.log("Code is op...")
-
 var a = prompt("Enter your number")
 var isTrue = confirm("Are you sure you want to leave this page and danger your computer ")
 if(isTrue){
@@ -16,3 +15,4 @@ else{
 console.log("Your number is " + a)
 
 document.title = "Hey I am good"
+
