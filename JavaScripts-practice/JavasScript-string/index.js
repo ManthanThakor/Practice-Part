2543421f08674119c71170fff6419cc3ec2b0907
Manthan Thakor <thakormanthan849@gmail.com>
2024-03-20 11:37:46 +0530

    string1

---
+++ JavaScripts-practice/JavasScript-string/index.js
@@ -0,0 +1,37 @@
+console.log("This is strings tutorial")
+let a = "Manthan";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a[6]);
+
+console.log(a.length)
+
+let real_name = "Manthan"
+let friend = "Raj"
+console.log("His name is " + real_name + " and his friends name is " + friend)
+console.log(`His name is ${real_name} and his friends name is ${friend}`)
+
+let c = "sbot"
+let d = "dkiller"
+let x = (c.slice(1,4));
+let z = (d.slice(1, 7)); 
+
+console.log(x +" "+ z); 
+
+
+
+let b = "ManthuRocks"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase()) 
+console.log(b.length) 
+console.log(b.slice(1, 5)) 
+console.log(b.slice(1)) 
+
+console.log(b.replace("th", "77"))
+console.log(b.concat(a, "gang", "aal", "ssda"))
+
+console.log(b)
